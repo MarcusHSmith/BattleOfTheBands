5a2dd620b98ed946659f528627af8a1057a2518c
Marcus Smith <mail4marcus@comcast.net>
2014-01-25 01:06:16 -0800

    Initialized repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BattleOfTheBands::Application.config.secret_token = 'a51c926cfe9c318c6efbb19cafc3ea88d508cb641d9eb84afee45e07a3c1397ffb9afd388e16994b4e46176a4ad0b0f9c38819291fe124a89fd53235006a9374'
